920bcee09fcd845677467a725f18ab86049c2338
wangrui <wangr@qiyi.com>
2016-02-25 14:21:01 +0800

    add analysis

--- buildBlog.sh
+++ buildBlog.sh
@@ -8,14 +8,16 @@ git init
 git add .
 git commit -m "update at `date` "
 
-# git remote add origin git@github.com:tangqiaoboy/tangqiaoboy.github.com.git >> /dev/null 2>&1
-# echo "### Pushing to Github..."
-# git push origin master -f
-# echo "### Done"
+git remote add origin git@github.com:ourui/ourui.github.com.git >> /dev/null 2>&1
+echo "### Pushing to Github..."
+git push origin master:master -f
+echo "### Done"
 
 git remote add gitcafe_ourui git@gitcafe.com:ourui/ourui.git >> /dev/null 2>&1
 echo "### Pushing Pages to GitCafe..."
 git push gitcafe_ourui master:gitcafe-pages -f
+
+
 cd ..
 echo "### Backup Source to GitCafe..."
 git push gitcafe_ourui master:source 
