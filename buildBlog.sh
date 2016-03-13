089eec9a85a395357df1e9c0f92b2624eb0005a4
wangrui <wangr@qiyi.com>
2016-03-13 14:53:23 +0800

    modify build script

--- buildBlog.sh
+++ buildBlog.sh
@@ -8,19 +8,19 @@ git init
 git add .
 git commit -m "update at `date` "
 
+git remote add coding git@git.coding.net:ourui/ourui.git >> /dev/null 2>&1
+echo "### Pushing Pages to Coding..."
+git push coding master:coding-pages -f
+
+cd ..
+echo "### Backup Source to Coding..."
+git push coding master:source 
+
+echo "### Done"
+
+
 git remote add origin git@github.com:ourui/ourui.github.com.git >> /dev/null 2>&1
 echo "### Pushing to Github..."
 git push origin master:master -f
 echo "### Done"
 
-git remote add gitcafe_ourui git@gitcafe.com:ourui/ourui.git >> /dev/null 2>&1
-echo "### Pushing Pages to GitCafe..."
-git push gitcafe_ourui master:gitcafe-pages -f
-
-
-cd ..
-echo "### Backup Source to GitCafe..."
-git push gitcafe_ourui master:source 
-
-
-echo "### Done"
